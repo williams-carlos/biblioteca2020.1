b8670a1c0169a890a40f4157fb0f816e5204240d
williams-carlos <jr_oficinag3@hotmail.com>
2020-09-04 10:57:08 -0300

    git ignore e teste.java

---
+++ src/main/java/br/com/biblioteca/Teste.java
@@ -0,0 +1,11 @@
+package br.com.biblioteca;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ola biblioteca");
+
+	}
+
+}
